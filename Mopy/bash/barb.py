LePresidente/wrye-bash

commit ad5815d9bf95faf0b77f7541d03733dd9eefb5ed

Squeezed more performance out of backup settings:
barb.py needs simplification - ideally bin BaseBackupSettings

Mopy/bash/barb.py
@@ -44,6 +44,8 @@ class BackupCancelled(BoltError):
 
 #------------------------------------------------------------------------------
 class BaseBackupSettings:
+    verApp = bass.AppVersion
+
     def __init__(self, parent=None, path=None, quit=False):
         if path is not None and path.ext == u'' and not path.exists():
             path = None
@@ -57,7 +59,6 @@ class BaseBackupSettings:
             self.archive = path.tail
         self.parent = parent
         self.verDat = bosh.settings['bash.version']
-        self.verApp = bass.AppVersion
         self.files = {}
         self.tmp = None
 
@@ -75,7 +76,8 @@ class BaseBackupSettings:
     def PromptFile(self):
         raise AbstractError
 
-    def PromptConfirm(self,msg=None):
+    @staticmethod
+    def PromptConfirm(msg=None):
         raise AbstractError
 
     def PromptMismatch(self):
@@ -84,16 +86,12 @@ class BaseBackupSettings:
     def CmpDataVersion(self):
         return cmp(self.verDat, bosh.settings['bash.version'])
 
-    def CmpAppVersion(self):
-        # Changed to prompt updating on any version change
-        # Needs to check the cached value in settings for the initial upgrade check
-        return cmp(self.verApp, bosh.settings['bash.version'])
-
     def SameDataVersion(self):
         return not self.CmpDataVersion()
 
-    def SameAppVersion(self):
-        return not self.CmpAppVersion()
+    @staticmethod
+    def SameAppVersion():
+        return not cmp(bass.AppVersion, bosh.settings['bash.version'])
 
 #------------------------------------------------------------------------------
 class BackupSettings(BaseBackupSettings):
@@ -212,17 +210,19 @@ class BackupSettings(BaseBackupSettings):
         self.maketmp()
         return True
 
-    def PromptConfirm(self,msg=None):
+    @staticmethod
+    def PromptConfirm(msg=None):
         msg = msg or _(u'Do you want to backup your Bash settings now?')
-        return askYes(self.parent,msg,_(u'Backup Bash Settings?'))
+        return askYes(Link.Frame, msg, _(u'Backup Bash Settings?'))
 
-    def PromptMismatch(self):
+    @staticmethod
+    def PromptMismatch():
         #returns False if same app version or old version == 0 (as in not previously installed) or user cancels
         if bosh.settings['bash.version'] == 0: return False
-        return not self.SameAppVersion() and self.PromptConfirm(
+        return not BaseBackupSettings.SameAppVersion() and BackupSettings.PromptConfirm(
             _(u'A different version of Wrye Bash was previously installed.')+u'\n' +
             _(u'Previous Version: ')+(u'%s\n' % bosh.settings['bash.version']) +
-            _(u'Current Version: ')+(u'%s\n' % self.verApp) +
+            _(u'Current Version: ')+(u'%s\n' % bass.AppVersion) +
             _(u'Do you want to create a backup of your Bash settings before they are overwritten?'))
 
     def PromptContinue(self):
@@ -352,15 +352,16 @@ class RestoreSettings(BaseBackupSettings):
         self.maketmp()
         return True
 
-    def PromptConfirm(self,msg=None):
+    @staticmethod
+    def PromptConfirm(msg=None):
         # returns False if user cancels
         msg = msg or _(u'Do you want to restore your Bash settings from a backup?')
         msg += u'\n\n' + _(u'This will force a restart of Wrye Bash once your settings are restored.')
-        return askYes(self.parent,msg,_(u'Restore Bash Settings?'))
+        return askYes(Link.Frame, msg, _(u'Restore Bash Settings?'))
 
     def PromptMismatch(self):
         # return True if same app version or user confirms
-        return self.SameAppVersion() or askWarning(self.parent,
+        return BaseBackupSettings.SameAppVersion() or askWarning(self.parent,
               _(u'The version of Bash used to create the selected backup file does not match the current Bash version!')+u'\n' +
               _(u'Backup v%s does not match v%s') % (self.verApp, bosh.settings['bash.version']) + u'\n' +
               u'\n' +

Mopy/bash/bash.py
@@ -67,8 +67,9 @@ def cmdBackup():
     path = None
     quit = opts.backup and opts.quietquit
     if opts.backup: path = GPath(opts.filename)
-    backup = barb.BackupSettings(balt.Link.Frame,path,quit,opts.backup_images)
-    if backup.PromptMismatch() or opts.backup:
+    if barb.BackupSettings.PromptMismatch() or opts.backup:
+        backup = barb.BackupSettings(balt.Link.Frame, path, quit,
+                                     opts.backup_images)
         try:
             backup.Apply()
         except bolt.StateError:
@@ -79,7 +80,7 @@ def cmdBackup():
         except barb.BackupCancelled:
             if not backup.SameAppVersion() and not backup.PromptContinue():
                 return False
-    del backup
+        del backup
     return quit
 
 def cmdRestore():
@@ -404,9 +405,9 @@ def main():
 
     # process backup/restore options
     # quit if either is true, but only after calling both
-    quit = cmdBackup() ##: takes time
-    quit = cmdRestore() or quit
-    if quit: return
+    quit_ = cmdBackup()
+    quit_ = cmdRestore() or quit_
+    if quit_: return
 
     basher.isUAC = isUAC
     if isUAC:

Mopy/bash/basher/settings_links.py
@@ -96,14 +96,14 @@ class Settings_RestoreSettings(ItemLink):
 
     @balt.conversation
     def Execute(self,event):
+        if not barb.RestoreSettings.PromptConfirm(): return
         backup = None # barb.RestoreSettings may raise....
         try:
             backup = barb.RestoreSettings(Link.Frame)
-            if backup.PromptConfirm():
-                backup.restore_images = balt.askYes(Link.Frame,
+            backup.restore_images = balt.askYes(Link.Frame,
                 _(u'Do you want to restore saved images as well as settings?'),
                 _(u'Restore Settings'))
-                with balt.BusyCursor(): backup.Apply()
+            with balt.BusyCursor(): backup.Apply()
         except bolt.StateError:
             deprint(u'Restore settings failed:', traceback=True)
             if backup: backup.WarnFailed()

commit 0d735d8fcda599e3f640ec8b665f169de144094a

Small fixups - WIP:
EEE comments/docs TTT

Regression introduced in ad5815d9bf95faf0b77f7541d03733dd9eefb5ed: EEE

verApp had a different meaning in those classes - renamed for clarity -
note ErrorConflict compared different versions than it displayed on error
message - check CmpDataVersion(self): in that commit. Cleaned this up for
good

Investigate if we can load newer settings (we should be mostly able) -
once # 390 is fixed that is.

Mopy/bash/barb.py
@@ -22,7 +22,18 @@
 #
 # =============================================================================
 
-"""Rollback library."""
+"""Rollback library.
+
+Re: bass.AppVersion, bass.settings['bash.version']
+
+The latter is read from the settings - so on upgrading Bash they are different,
+whereupon is based the backup-on-upgrade routine. Later on
+bass.settings['bash.version'] is set to bass.AppVersion. We save both in the
+settings we backup:
+- bass.settings['bash.version'] is saved first and corresponds to the version
+the settings were created with
+- bass.AppVersion is the version of Bash currently executing the backup
+"""
 
 import cPickle
 from os.path import join as jo
@@ -95,7 +106,6 @@ class BaseBackupSettings:
             self._dir = path.head
             self.archive = path.tail
         self.parent = parent
-        self.verDat = bass.settings['bash.version']
         self.files = {}
 
     def Apply(self):
@@ -174,7 +184,7 @@ class BackupSettings(BaseBackupSettings):
             with temp_dir.join(u'backup.dat').open('wb') as out:
                 # data version, if this doesn't match the installed data
                 # version, do not allow restore
-                cPickle.dump(self.verDat, out, -1)
+                cPickle.dump(bass.settings['bash.version'], out, -1) # Bash version the settings were saved with
                 # app version, if this doesn't match the installer app version,
                 # warn the user on restore
                 cPickle.dump(bass.AppVersion, out, -1)
@@ -193,7 +203,7 @@ class BackupSettings(BaseBackupSettings):
         """Prompt for backup filename - return False if user cancels."""
         if self.archive is None or self._dir.join(self.archive).exists():
             filename = u'Backup Bash Settings %s (%s) v%s-%s.7z' % (
-                bush.game.fsName, bolt.timestamp(), self.verDat,
+                bush.game.fsName, bolt.timestamp(), bass.settings['bash.version'],
                 bass.AppVersion)
             if not self.quit:
                 path = askSave(self.parent, title=_(u'Backup Bash Settings'),
@@ -232,12 +242,24 @@ class RestoreSettings(BaseBackupSettings):
         command = archives.extractCommand(self._dir.join(self.archive), temp_dir)
         archives.extract7z(command, self._dir.join(self.archive))
         with temp_dir.join(u'backup.dat').open('rb') as ins:
-            self.verDat = cPickle.load(ins)
-            self.verApp = cPickle.load(ins)
-        if self.ErrorConflict():
+            saved_settings_version = cPickle.load(ins) # version of Bash that created the backed up settings
+            settings_saved_with = cPickle.load(ins) # version of Bash that created the backup
+        if saved_settings_version > bass.settings['bash.version']:
+            # Disallow restoring settings saved on a newer version of bash # FIXME drop
+            showError(self.parent,
+                  _(u'The data format of the selected backup file is newer than the current Bash version!')+u'\n' +
+                  _(u'Backup v%s is not compatible with v%s') % (saved_settings_version, bass.settings['bash.version']) + u'\n' +
+                  u'\n' +
+                  _(u'You cannot use this backup with this version of Bash.'),
+                  _(u'Error: Settings are from newer Bash version'))
             self.WarnFailed()
             return
-        elif not self.PromptMismatch():
+        elif not (settings_saved_with == bass.settings['bash.version'] or askWarning(self.parent,
+              _(u'The version of Bash used to create the selected backup file does not match the current Bash version!')+u'\n' +
+              _(u'Backup v%s does not match v%s') % (settings_saved_with, bass.settings['bash.version']) + u'\n' +
+              u'\n' +
+              _(u'Do you want to restore this backup anyway?'),
+              _(u'Warning: Version Mismatch!'))):
             raise BackupCancelled()
 
         deprint(u'')
@@ -293,27 +315,6 @@ class RestoreSettings(BaseBackupSettings):
             self._dir = path.head
             self.archive = path.tail
         return True
-
-    def PromptMismatch(self):
-        # return True if same app version or user confirms
-        return SameAppVersion() or askWarning(self.parent,
-              _(u'The version of Bash used to create the selected backup file does not match the current Bash version!')+u'\n' +
-              _(u'Backup v%s does not match v%s') % (self.verApp, bass.settings['bash.version']) + u'\n' +
-              u'\n' +
-              _(u'Do you want to restore this backup anyway?'),
-              _(u'Warning: Version Mismatch!'))
-
-    def ErrorConflict(self):
-        # returns positive if the settings are from a newer Bash version
-        if cmp(self.verDat, bass.settings['bash.version']) > 0:
-            showError(self.parent,
-                  _(u'The data format of the selected backup file is newer than the current Bash version!')+u'\n' +
-                  _(u'Backup v%s is not compatible with v%s') % (self.verApp, bass.settings['bash.version']) + u'\n' +
-                  u'\n' +
-                  _(u'You cannot use this backup with this version of Bash.'),
-                  _(u'Error: Version Conflict!'))
-            return True
-        return False
 
     def WarnFailed(self):
         showWarning(self.parent,

Mopy/bash/basher/__init__.py
@@ -3716,8 +3716,7 @@ class BashFrame(BaltFrame):
             title = bush.game.altName + u' %s%s'
         else:
             title = u'Wrye Bash %s%s '+_(u'for')+u' '+bush.game.displayName
-        title %= (settings['bash.version'],
-                  (u' ' + _(u'(Standalone)')) if settings[
+        title %= (bass.AppVersion, (u' ' + _(u'(Standalone)')) if settings[
                       'bash.standalone'] else u'')
         if CBashApi.Enabled:
             title += u', CBash %s: ' % (CBashApi.VersionText,)

Mopy/bash/basher/constants.py
@@ -148,7 +148,6 @@ colorInfo = {
 settingDefaults = { ##: (178) belongs to bosh (or better to a settings package)
     #--Basics
     'bash.version': 0,
-    'bash.readme': (0,u'0'),
     'bash.CBashEnabled': True,
     'bash.backupPath': None,
     'bash.frameMax': False, # True if maximized

Mopy/bash/bosh/__init__.py
@@ -3441,6 +3441,3 @@ def initSettings(readOnly=False, _dat=u'BashSettings.dat',
     # No longer pulling version out of the readme, but still need the old
     # cached value for upgrade check! (!)
     bass.settings = settings
-    if 'bash.readme' in settings:
-        settings['bash.version'] = _(settings['bash.readme'][1])
-        del settings['bash.readme']